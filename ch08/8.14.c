0914ee9eabd2e6fb6dcb1f5a4a8773c517a632ee
JunZhong Qin <qinjunzhong@gmail.com>
2015-04-04 11:25:34 -0400

    add 8.14.c

---
+++ ch08/8.14.c
@@ -0,0 +1,14 @@
+#include "apue.h"
+#include "system.h"
+
+int main(int argc, char *argv[]) {
+    int status;
+    if (argc < 2) {
+        err_sys("command-line argument required");
+    }
+
+    if ((status = system(argv[1])) < 0)
+        err_sys("system() error");
+    pr_exit(status);
+    exit(0);
+}
